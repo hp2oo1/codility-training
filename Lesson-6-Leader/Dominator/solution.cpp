6bb1d717e2884f25ef209b976c4862aabd313df6
hp2oo1 <huangpeng723@gmail.com>
2015-05-31 23:34:39 +0100

    Update solution.cpp

--- Lesson-6-Leader/Dominator/solution.cpp
+++ Lesson-6-Leader/Dominator/solution.cpp
@@ -22,3 +22,42 @@ expected worst-case space complexity is O(1), beyond input storage (not counting
 Elements of input arrays can be modified.
 Copyright 2009–2015 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
 */
+
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(vector<int> &A) {
+    // write your code in C++11
+    int size(0);
+    int value(0);
+    for(unsigned int i=0; i<A.size(); ++i) {
+        if( size==0 ) {
+            size++;
+            value = A[i];
+        }
+        else {
+            if( A[i]==value )
+                size++;
+            else
+                size--;
+        }
+    }
+    if( size<=0 )
+        return -1;
+    
+    int count(0);
+    int j(-1);
+    for(unsigned int i=0; i<A.size(); ++i) {
+        if( A[i]==value ) {
+            count++;
+            j = i;
+        }
+    }
+    if( count<=A.size()/2 )
+        return -1;
+    else
+        return j;
+}
