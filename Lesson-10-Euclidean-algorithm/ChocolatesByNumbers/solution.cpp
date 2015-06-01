6e1b8b519cbd9e408eb57c0821683692514cde6f
hp2oo1 <huangpeng723@gmail.com>
2015-06-01 20:44:38 +0100

    Update solution.cpp

--- Lesson-10-Euclidean-algorithm/ChocolatesByNumbers/solution.cpp
+++ Lesson-10-Euclidean-algorithm/ChocolatesByNumbers/solution.cpp
@@ -1,19 +1,27 @@
-/*
-Two positive integers N and M are given. Integer N represents the number of chocolates arranged in a circle, numbered from 0 to N − 1.
-You start to eat the chocolates. After eating a chocolate you leave only a wrapper.
-You begin with eating chocolate number 0. Then you omit the next M − 1 chocolates or wrappers on the circle, and eat the following one.
-More precisely, if you ate chocolate number X, then you will next eat the chocolate with number (X + M) modulo N (remainder of division).
-You stop eating when you encounter an empty wrapper.
-For example, given integers N = 10 and M = 4. You will eat the following chocolates: 0, 4, 8, 2, 6.
-The goal is to count the number of chocolates that you will eat, following the above rules.
-Write a function:
-int solution(int N, int M);
-that, given two positive integers N and M, returns the number of chocolates that you will eat.
-For example, given integers N = 10 and M = 4. the function should return 5, as explained above.
-Assume that:
-N and M are integers within the range [1..1,000,000,000].
-Complexity:
-expected worst-case time complexity is O(log(N+M));
-expected worst-case space complexity is O(log(N+M)).
-Copyright 2009–2015 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
-*/
+// you can use includes, for example:
+// #include <algorithm>
+#include <vector>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+// greatest common divisor
+int gcd(int a, int b) {
+    int c;
+    while( a!=0 ) {
+        c = a;
+        a = b%a;
+        b = c;
+    }
+    return b;
+}
+
+// least common multiple
+long lcm(long a, long b) {
+    return a*b/gcd(a,b);
+}
+
+int solution(int N, int M) {
+    // write your code in C++11
+    return lcm(N,M)/M;
+}
