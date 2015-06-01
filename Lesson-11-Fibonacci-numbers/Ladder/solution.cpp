c80f9520df1f9a6569d80a5bcc65d21a9f895efa
hp2oo1 <huangpeng723@gmail.com>
2015-06-01 22:42:48 +0100

    Update solution.cpp

--- Lesson-11-Fibonacci-numbers/Ladder/solution.cpp
+++ Lesson-11-Fibonacci-numbers/Ladder/solution.cpp
@@ -1,64 +1,31 @@
 /*
-You have to climb up a ladder. The ladder has exactly N rungs, numbered from 1 to N. With each step, you can ascend by one or two rungs. More precisely:
-
-with your first step you can stand on rung 1 or 2,
-if you are on rung K, you can move to rungs K + 1 or K + 2,
-finally you have to stand on rung N.
-Your task is to count the number of different ways of climbing to the top of the ladder.
-
-For example, given N = 4, you have five different ways of climbing, ascending by:
-
-1, 1, 1 and 1 rung,
-1, 1 and 2 rungs,
-1, 2 and 1 rung,
-2, 1 and 1 rungs, and
-2 and 2 rungs.
-Given N = 5, you have eight different ways of climbing, ascending by:
-
-1, 1, 1, 1 and 1 rung,
-1, 1, 1 and 2 rungs,
-1, 1, 2 and 1 rung,
-1, 2, 1 and 1 rung,
-1, 2 and 2 rungs,
-2, 1, 1 and 1 rungs,
-2, 1 and 2 rungs, and
-2, 2 and 1 rung.
-The number of different ways can be very large, so it is sufficient to return the result modulo 2P, for a given integer P.
-
-Assume that the following declarations are given:
-
-struct Results {
-  int * C;
-  int L;
-};
-
-Write a function:
-
-struct Results solution(int A[], int B[], int L);
-
-that, given two non-empty zero-indexed arrays A and B of L integers, returns an array consisting of L integers specifying the consecutive answers; position I should contain the number of different ways of climbing the ladder with A[I] rungs modulo 2B[I].
-
-For example, given L = 5 and:
-
-    A[0] = 4   B[0] = 3
-    A[1] = 4   B[1] = 2
-    A[2] = 5   B[2] = 4
-    A[3] = 5   B[3] = 3
-    A[4] = 1   B[4] = 1
-the function should return the sequence [5, 1, 8, 0, 1], as explained above.
-
-Assume that:
-
-L is an integer within the range [1..30,000];
-each element of array A is an integer within the range [1..L];
-each element of array B is an integer within the range [1..30].
-Complexity:
-
-expected worst-case time complexity is O(L);
-expected worst-case space complexity is O(L), beyond input storage (not counting the storage required for input arguments).
-Elements of input arrays can be modified.
-
-Copyright 2009–2015 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
+example 
+example test	0.009 s	OK
+Correctness tests
+extreme 
+extreme small values	0.009 s	OK
+small_functional 
+small functional	0.009 s	OK
+small 
+small tests	0.009 s	OK
+small_random 
+small random, length = ~100	0.009 s	WRONG ANSWER 
+got ..151, 7380601, 1175, -575, 304728, 267914.. expected ..151, 7380601, 1175, 7617, 304728, 267914..
+Performance tests
+medium_random
+medium random, length = ~1,000	0.009 s	WRONG ANSWER 
+got [-27, 23456, 5, -1875.. expected [485, 23456, 5, 173, ..
+large_range 
+large range, length = ~30,000	0.026 s	WRONG ANSWER 
+got .., 30840, 5, 839613, -6398, 784063, -15, .. expected .., 30840, 5, 839613, 1794, 784063, 1, 524..
+large_random 
+large random, length = ~30,000	0.026 s	WRONG ANSWER 
+got [93148797, -18980743, 94924585,.. expected [93148797, 14573689, 94924585, ..
+extreme_large 
+all max size of the ladder	0.027 s	RUNTIME ERROR 
+tested program terminated unexpectedly
+stderr:
+Error during output processing. Make sure your program returns a value of correct type.
 */
 
 // you can use includes, for example:
