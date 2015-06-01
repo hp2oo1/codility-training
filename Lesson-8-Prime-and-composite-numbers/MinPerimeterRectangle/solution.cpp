8ec567931ba89deef46cee7cd8a47ed429564adb
hp2oo1 <huangpeng723@gmail.com>
2015-06-01 15:29:51 +0100

    Update solution.cpp

--- Lesson-8-Prime-and-composite-numbers/MinPerimeterRectangle/solution.cpp
+++ Lesson-8-Prime-and-composite-numbers/MinPerimeterRectangle/solution.cpp
@@ -7,7 +7,7 @@
 int solution(int N) {
     // write your code in C++11
     float sqrt_N = sqrt(N);
-    for(int a=floor(sqrt_N)+1; a>0; a--) {
+    for(int a=floor(sqrt_N); a>0; a--) {
         if( N%a==0 ) {
             return 2*(a+N/a);
         }
