29959b38f291fe3e126af218d30271ef555379b2
hp2oo1 <huangpeng723@gmail.com>
2015-06-01 15:31:26 +0100

    Update solution.cpp

--- Lesson-8-Prime-and-composite-numbers/CountFactors/solution.cpp
+++ Lesson-8-Prime-and-composite-numbers/CountFactors/solution.cpp
@@ -1,14 +1,20 @@
-/*
-A positive integer D is a factor of a positive integer N if there exists an integer M such that N = D * M.
-For example, 6 is a factor of 24, because M = 4 satisfies the above condition (24 = 6 * 4).
-Write a function:
-int solution(int N);
-that, given a positive integer N, returns the number of its factors.
-For example, given N = 24, the function should return 8, because 24 has 8 factors, namely 1, 2, 3, 4, 6, 8, 12, 24. There are no other factors of 24.
-Assume that:
-N is an integer within the range [1..2,147,483,647].
-Complexity:
-expected worst-case time complexity is O(sqrt(N));
-expected worst-case space complexity is O(1).
-Copyright 2009–2015 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
-*/
+// you can use includes, for example:
+#include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int solution(int N) {
+    // write your code in C++11
+    float sqrt_N = sqrt(N);
+    int count(0);
+    for(int a=floor(sqrt_N); a>0; a--) {
+        if( N%a==0 ) {
+            count++;
+        }
+    }
+    if( N==floor(sqrt_N)*floor(sqrt_N) )
+        return 2*count-1;
+    else
+        return 2*count;
+}
