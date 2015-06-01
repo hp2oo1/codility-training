87a2d50e99519918887a824727c70606570cd891
hp2oo1 <huangpeng723@gmail.com>
2015-06-01 16:52:49 +0100

    Update solution.cpp

--- Lesson-9-Sieve-of-Eratosthenes/CountSemiprimes/solution.cpp
+++ Lesson-9-Sieve-of-Eratosthenes/CountSemiprimes/solution.cpp
@@ -1,32 +1,87 @@
-/*
-A prime is a positive integer X that has exactly two distinct divisors: 1 and X. The first few prime integers are 2, 3, 5, 7, 11 and 13.
-A semiprime is a natural number that is the product of two (not necessarily distinct) prime numbers. The first few semiprimes are 4, 6, 9, 10, 14, 15, 21, 22, 25, 26.
-You are given two non-empty zero-indexed arrays P and Q, each consisting of M integers. These arrays represent queries about the number of semiprimes within specified ranges.
-Query K requires you to find the number of semiprimes within the range (P[K], Q[K]), where 1 ≤ P[K] ≤ Q[K] ≤ N.
-For example, consider an integer N = 26 and arrays P, Q such that:
-    P[0] = 1    Q[0] = 26
-    P[1] = 4    Q[1] = 10
-    P[2] = 16   Q[2] = 20
-The number of semiprimes within each of these ranges is as follows:
-(1, 26) is 10,
-(4, 10) is 4,
-(16, 20) is 0.
-Write a function:
-vector<int> solution(int N, vector<int> &P, vector<int> &Q);
-that, given an integer N and two non-empty zero-indexed arrays P and Q consisting of M integers, returns an array consisting of M elements specifying the consecutive answers to all the queries.
-For example, given an integer N = 26 and arrays P, Q such that:
-    P[0] = 1    Q[0] = 26
-    P[1] = 4    Q[1] = 10
-    P[2] = 16   Q[2] = 20
-the function should return the values [10, 4, 0], as explained above.
-Assume that:
-N is an integer within the range [1..50,000];
-M is an integer within the range [1..30,000];
-each element of arrays P, Q is an integer within the range [1..N];
-P[i] ≤ Q[i].
-Complexity:
-expected worst-case time complexity is O(N*log(log(N))+M);
-expected worst-case space complexity is O(N+M), beyond input storage (not counting the storage required for input arguments).
-Elements of input arrays can be modified.
-Copyright 2009–2015 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
-*/
+// you can use includes, for example:
+#include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int findMin(vector<int> &P) {
+    int minP(P[0]);
+    for(unsigned int i=0; i<P.size(); ++i) {
+        if( P[i]<minP )
+            minP = P[i];
+    }
+    return minP;
+}
+
+int findMax(vector<int> &Q) {
+    int maxQ(Q[0]);
+    for(unsigned int i=0; i<Q.size(); ++i) {
+        if( Q[i]>maxQ )
+            maxQ = Q[i];
+    }
+    return maxQ;
+}
+
+bool isPrime(int number) {
+    int a = floor(sqrt(number));
+    for(unsigned int i=a; i>1; --i) {
+        if( number%i==0 )
+            return false;
+    }
+    return true;
+}
+
+vector<int> findPrimes(int first, int last) {
+    vector<int> primes;
+    for(unsigned int i=first; i<=last; ++i) {
+        if( isPrime(i) ) {
+            primes.push_back(i);
+            // cout << i << endl;
+        }
+    }
+    return primes;
+}
+
+vector<int> findSemiPrimes(vector<int> &primes, int first, int last) {
+    vector<int> semiPrimes;
+    for(unsigned int i=0; i<primes.size(); ++i) {
+        for(unsigned int j=0; j<primes.size(); ++j) {
+            if( primes[i]<=primes[j] ) {
+                int semiPrime = primes[i]*primes[j];
+                if( semiPrime>=first && semiPrime<=last ) {
+                    semiPrimes.push_back( semiPrime );
+                    // cout << semiPrime << endl;
+                }
+            }
+        }
+    }
+    return semiPrimes;
+}
+
+int countSemiPrimes(vector<int> &semiPrimes, int first, int last) {
+    int count(0);
+    for(unsigned int i=0; i<semiPrimes.size(); ++i) {
+        // cout << semiPrimes[i] << endl;
+        if( semiPrimes[i]>=first && semiPrimes[i]<=last )
+            count++;
+    }
+    return count;
+}
+
+vector<int> solution(int N, vector<int> &P, vector<int> &Q) {
+    // write your code in C++11
+    int minP = findMin(P);
+    int maxQ = findMax(Q);
+    // cout << minP << " " << maxQ << endl;
+    
+    vector<int> primes = findPrimes(2,maxQ);
+    // cout << endl;
+    vector<int> semiPrimes = findSemiPrimes(primes,minP,maxQ);
+    
+    vector<int> counts(P.size(),0);
+    for(unsigned int i=0; i<P.size(); ++i) {
+        counts[i] = countSemiPrimes(semiPrimes,P[i],Q[i]);
+    }
+    
+    return counts;
+}
