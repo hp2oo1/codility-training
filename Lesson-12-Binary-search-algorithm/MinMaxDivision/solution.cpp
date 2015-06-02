e711f9222d74c5ec83e592bb2a77287cf331075c
hp2oo1 <huangpeng723@gmail.com>
2015-06-02 21:16:16 +0100

    Update solution.cpp

--- Lesson-12-Binary-search-algorithm/MinMaxDivision/solution.cpp
+++ Lesson-12-Binary-search-algorithm/MinMaxDivision/solution.cpp
@@ -1,41 +1,44 @@
-/*
-You are given integers K, M and a non-empty zero-indexed array A consisting of N integers. Every element of the array is not greater than M.
-You should divide this array into K blocks of consecutive elements. The size of the block is any integer between 0 and N. Every element of the array should belong to some block.
-The sum of the block from X to Y equals A[X] + A[X + 1] + ... + A[Y]. The sum of empty block equals 0.
-The large sum is the maximal sum of any block.
-For example, you are given integers K = 3, M = 5 and array A such that:
-  A[0] = 2
-  A[1] = 1
-  A[2] = 5
-  A[3] = 1
-  A[4] = 2
-  A[5] = 2
-  A[6] = 2
-The array can be divided, for example, into the following blocks:
-[2, 1, 5, 1, 2, 2, 2], [], [] with a large sum of 15;
-[2], [1, 5, 1, 2], [2, 2] with a large sum of 9;
-[2, 1, 5], [], [1, 2, 2, 2] with a large sum of 8;
-[2, 1], [5, 1], [2, 2, 2] with a large sum of 6.
-The goal is to minimize the large sum. In the above example, 6 is the minimal large sum.
-Write a function:
-int solution(int K, int M, vector<int> &A);
-that, given integers K, M and a non-empty zero-indexed array A consisting of N integers, returns the minimal large sum.
-For example, given K = 3, M = 5 and array A such that:
-  A[0] = 2
-  A[1] = 1
-  A[2] = 5
-  A[3] = 1
-  A[4] = 2
-  A[5] = 2
-  A[6] = 2
-the function should return 6, as explained above.
-Assume that:
-N and K are integers within the range [1..100,000];
-M is an integer within the range [0..10,000];
-each element of array A is an integer within the range [0..M].
-Complexity:
-expected worst-case time complexity is O(N*log(N+M));
-expected worst-case space complexity is O(1), beyond input storage (not counting the storage required for input arguments).
-Elements of input arrays can be modified.
-Copyright 2009–2015 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
-*/
+// you can use includes, for example:
+// #include <algorithm>
+
+// you can write to stdout for debugging purposes, e.g.
+// cout << "this is a debug message" << endl;
+
+int process(int mid, vector<int> &A) {
+    int k(1);
+    int sum(0);
+    for(unsigned int i=0; i<A.size(); ++i) {
+        sum += A[i];
+        if( sum>mid ) {
+            sum = A[i];
+            k++;
+        }
+    }
+    return k;
+}
+
+int solution(int K, int M, vector<int> &A) {
+    // write your code in C++11
+    int lower_bound(A[0]);
+    int upper_bound(0);
+    for(unsigned int i=0; i<A.size(); ++i) {
+        lower_bound  = max(lower_bound,A[i]);
+        upper_bound += A[i];
+    }
+    
+    int result(-1);
+    while( lower_bound<=upper_bound ) {
+        int mid = (lower_bound + upper_bound) / 2;
+        cout << lower_bound << " " << mid << " " << upper_bound;
+        int k = process(mid,A);
+        if( k<=K ) {
+            upper_bound = mid-1;
+            result = mid;
+        }
+        else {
+            lower_bound = mid+1;
+        }
+        // cout << " " << k << " " << result << endl;
+    }
+    return result;
+}
