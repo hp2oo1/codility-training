8a7e9ae8ff582a60ac3e65f7a393fca529fc886d
hp2oo1 <huangpeng723@gmail.com>
2015-06-07 00:03:26 +0100

    Update solution.cpp

--- Lesson-13-Caterpillar-method/CountDistinctSlices/solution.cpp
+++ Lesson-13-Caterpillar-method/CountDistinctSlices/solution.cpp
@@ -15,42 +15,49 @@
 // cout << "this is a debug message" << endl;
 
 int solution(int M, vector<int> &A) {
-    // for(unsigned int i=0; i<A.size(); ++i)
-        // cout << A[i] << " ";
-    // cout << endl;
     // write your code in C++11
-    long i(0);
-    long j(0);
-    long k(0);
-    long count(0);
-    bitset<100001> mybitset;
-    vector<long> myvector(100001,0);
+    long i(0); // back index
+    long j(0); // front index
+    long k(0); // new back index
+    long count(0); // count of distinct slices
+    bitset<100001> mybitset; // default all bits as 0 (unvisited)
+    vector<long> myvector(100001,-1);
     while( i<A.size() ) {
-        // cout << i << " " << j << " ";
         if( j<A.size() ) {
+            // if A[j] not visited before
             if( mybitset[A[j]]==0 ) {
+                // mark it as visited
                 mybitset[A[j]]= 1;
+                // record its index
                 myvector[A[j]]= j;
+                // move front index to next one
                 j++;
             }
             else {
+                // k is the index of previous A[j] appears
                 k=myvector[A[j]];
-                // cout << i << " " << k << " " << j << endl;
+                // the total number of slices between i and j
                 count += (j-i+2)*(j-i+1)/2;
+                // minus the number of slices which are not distinct
                 count -= k-i+1;
+                // minus the number of slices which will be repeated once i updates to k+1
                 count -= (j-k+1)*(j-k)/2;
+                // update i to k+1
                 i=k+1;
+                // reset j to i
                 j=i;
+                // reset all bits as 0 (unvisited)
                 mybitset.reset();
             }
         }
         else {
+            // when j reaches the end, count last distinct slices
             count += (j-i+1)*(j-i)/2;
+            // update i reaches the end
             i=j;
         }
         if( count>1000000000 )
             return 1000000000;
-        // cout << count << endl;
     }
     return count;
 }
